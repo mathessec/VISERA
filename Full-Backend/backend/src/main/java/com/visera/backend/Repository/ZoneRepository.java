69d7df758ebb9e143c56e135736fdf49c93d688f
mathes-code <matheswaran@healthpilot.ai>
2025-12-23 23:40:41 +0530

    Remove Full-Backend folder from repository

--- Full-Backend/backend/src/main/java/com/visera/backend/Repository/ZoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.visera.backend.Repository;
-import com.visera.backend.Entity.Zone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ZoneRepository extends JpaRepository<Zone, Long> {
-}
-
--- Full-Backend/backend/src/main/resources/migration_weight_to_string.sql
+++
@@ -1,9 +0,0 @@
--- Migration: Change weight column from DOUBLE to VARCHAR
--- This allows storing weight exactly as entered (e.g., "100" instead of "100.0")
--- Run this script on your MySQL database: visera_db
-
-ALTER TABLE skus 
-MODIFY COLUMN weight VARCHAR(50) NULL;
-
--- Verify the change
-DESCRIBE skus;
